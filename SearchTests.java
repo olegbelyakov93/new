44d8b2e9e536de30346d72885fa781402e933ff1
olegbelyakov93 <125873076+olegbelyakov93@users.noreply.github.com>
2023-02-20 15:13:45 +0300

    Update SearchTests.java

--- SearchTests.java
+++ SearchTests.java
@@ -4,8 +4,7 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
-    //коммент
 }
 }
