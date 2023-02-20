c55157a10ef67e5316347465df87209aa3da8ef7
olegbelyakov93 <125873076+olegbelyakov93@users.noreply.github.com>
2023-02-20 13:34:09 +0300

    Update and rename 123 to .gitignore

---
+++ SearchTests.java
@@ -0,0 +1,10 @@
+public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
+}
